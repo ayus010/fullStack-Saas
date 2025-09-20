ef96ab0578ff5b4f81a822e3f74fc9015f7449a4
Ayush Kumar <ayushkumar85272@gmail.com>
2025-09-21 00:12:17 +0530

    feat: enhance authentication flow with new layout and views, add user management features

--- apps/web/app/(dashboard)/page.tsx
+++ apps/web/app/(dashboard)/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import { SignInButton, UserButton } from "@clerk/nextjs";
-import {useMutation, useQuery, Authenticated, Unauthenticated } from "convex/react";
+import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+import {useMutation, useQuery } from "convex/react";
 import { api } from "@workspace/backend/_generated/api";
 import { Button } from "@workspace/ui/components/button";
 
@@ -11,18 +11,17 @@ export default function Page() {
 
   return (
     <>
-      <Authenticated>
+
         <div className="flex items-center justify-center min-h-svh">
           <p>APPS/WEB</p>
           <UserButton />
+          <OrganizationSwitcher />
           <Button onClick={()=>addUser()}>Add</Button>
-          {JSON.stringify(user)}
+          <div className="max-w-sm w-full mx-auto">
+            {JSON.stringify(user, null, 2)}
+          </div>
         </div>
-      </Authenticated>
-      <Unauthenticated>
-        <p>Must be signed In</p>
-        <SignInButton>SignIn</SignInButton>
-      </Unauthenticated>
+
     </>
   )
 }
